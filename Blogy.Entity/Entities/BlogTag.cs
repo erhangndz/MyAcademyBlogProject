021814c418b0f49430d7d159df30de3d03e1f524
erhangndz <erhangndz@live.com>
2025-10-02 22:33:30 +0300

    updated

---
+++ Blogy.Entity/Entities/BlogTag.cs
@@ -0,0 +1,14 @@
+﻿using Blogy.Entity.Entities.Common;
+
+namespace Blogy.Entity.Entities
+{
+    public class BlogTag: BaseEntity
+    {
+        public int BlogId { get; set; }
+        public int TagId { get; set; }
+        public Blog Blog { get; set; }
+        public Tag Tag { get; set; }
+
+
+    }
+}
---
+++ Blogy.Entity/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿using Blogy.Entity.Entities.Common;
+
+namespace Blogy.Entity.Entities
+{
+    public class Category: BaseEntity
+    {
+        public string Name { get; set; }
+        public IList<Blog> Blogs { get; set; }
+
+
+    }
+}
